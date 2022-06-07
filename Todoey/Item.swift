43077fa563b5fac1e138ad9cb87d32b2f8373a15
uniextra <34218091+uniextra@users.noreply.github.com>
2022-06-07 18:31:20 +0200

    first part

---
+++ Todoey/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Martin Dn on 7/6/22.
+//  Copyright © 2022 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class Item: Encodable{
+    var title: String = ""
+    var done: Bool = false
+}
---
+++ Todoey/TodoListViewController.swift
@@ -0,0 +1,110 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Philipp Muellauer on 02/12/2019.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+    
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+
+    
+    var itemArray = [Item]()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+
+        let newItem = Item()
+        newItem.title = "hola"
+        itemArray.append(newItem)
+        // Cargo los items desde la memoria
+        
+        
+        
+//        if let items = defaults.array(forKey: "TodoListArray") as? [Item]{
+//            itemArray = items
+//        }
+
+    }
+    
+    //MARK: - Cantidad de rows
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    //MARK: - pongo los datos en la celda
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let message = itemArray[indexPath.row].title
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = message
+                
+        cell.accessoryType = itemArray[indexPath.row].done == true ? .checkmark : .none
+                
+        return cell
+    }
+    //MARK: - Celda seleccionada
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+        saveItems()
+        
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    
+    //MARK: - ADD new item
+    @IBAction func AddButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Item", message: "", preferredStyle: .alert)
+        
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            let newItem = Item()
+            newItem.title = textField.text!
+            
+            self.itemArray.append(newItem)
+            
+            self.saveItems()
+            
+            self.tableView.reloadData()
+            
+        }
+
+        alert.addTextField{ (alertTextField) in
+            alertTextField.placeholder = "New alert"
+            textField = alertTextField
+            
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true,completion: nil)
+    }
+    
+    
+    func saveItems(){
+        // Guardo los items en la memoria
+        let encoder = PropertyListEncoder()
+        
+        do{
+            let data = try encoder.encode(itemArray)
+            try data.write(to: dataFilePath!)
+        }catch{
+            print("error: \(error)")
+        }
+        tableView.reloadData()
+        
+    }
+}
+
